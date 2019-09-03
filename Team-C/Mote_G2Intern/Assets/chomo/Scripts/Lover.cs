97ef91528164b12e6fc9f4d21445982a90672fef
makia <g1821120@gmail.com>
2019-09-03 13:52:11 +0900

    Loverの停止処理を追加完了

--- Team-C/Mote_G2Intern/Assets/chomo/Scripts/Lover.cs
+++ Team-C/Mote_G2Intern/Assets/chomo/Scripts/Lover.cs
@@ -1,12 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UniRx;
+using System;
 
 public class Lover : MonoBehaviour
 {
     private LoverMover _loverMover;
 
     [SerializeField] private float m_moveSpeed = 100f;
+
+    [SerializeField] private double m_stopTime = 1.5;
+
+    private bool m_isStopped; 
 
     //呼び出し時に方向を代入させる
     [HideInInspector] public Vector2 m_moveDirection = Vector2.right;
@@ -18,21 +24,26 @@ public class Lover : MonoBehaviour
 
     private void Start()
     {
-       // LoverManager.Instance.AddList(this);
+        LoverManager.Instance.AddList(this);
+
+        m_isStopped = false;
     }
 
     private void LoverUpdate()
     {
+        /// <summary>
+        /// 停止フラグがtrueの場合、
+        /// 早期リターン
+        /// </summary>
+        if (m_isStopped)
+        {
+            return;
+        }
+
         _loverMover.Move(m_moveDirection * m_moveSpeed);
     }
 
-    private void FixedUpdate()
-    {
-        _loverMover.Move(m_moveDirection * m_moveSpeed);
-    }
 
-    #region テスト用
-    /*
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
@@ -41,9 +52,25 @@ public class Lover : MonoBehaviour
                            - transform.position;
 
             m_moveDirection = direction.normalized;
+
+            return;
+        }
+
+        /// <summary>
+        /// プレイヤーに一定の距離近づいたら、
+        /// Loverの動きをしばらく止める
+        /// </summary>
+        if (collision.gameObject.CompareTag("Stop"))
+        {
+            m_isStopped = true;
+
+            /// <summary>
+            /// m_stopTime秒経ったらfalseにする
+            /// </summary>
+            Observable.Timer(TimeSpan.FromSeconds(m_stopTime)).Subscribe(_ =>
+            {
+                m_isStopped = false;
+            }).AddTo(this);
         }
     }
-    */
-    #endregion
-
 }
