853029b007820deb36b1942828dab3c3633210be
haitatsuin <48271054+haitatsuin@users.noreply.github.com>
2019-09-05 04:46:09 +0900

    タイトルシーン作りこみ

--- Team-C/Mote_G2Intern/Assets/SatoMizu/Scripts/TitleButtonScript.cs
+++ Team-C/Mote_G2Intern/Assets/SatoMizu/Scripts/TitleButtonScript.cs
@@ -3,24 +3,57 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class TitleButtonScript : MonoBehaviour {
-
-    private SpriteRenderer m_spriteRender; 
+public class TitleButtonScript : MonoBehaviour
+{
+    [SerializeField]    private string m_SceneName;
+    private SpriteRenderer m_spriteRender;
+    private bool m_bReady;
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         m_spriteRender = GetComponent<SpriteRenderer>();
         var color = m_spriteRender.color;
         color.a = 0.5f;
         m_spriteRender.color = color;
+        m_bReady = false;
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    void Update()
+    {
+        if (!m_bReady)
+        {
+            return;
+        }
+
         if (Input.anyKey)
         {
             this.GetComponent<AudioManager>().AudioPlay();
-            SceneManager.LoadScene("MainScene");
+            SceneManager.LoadScene(m_SceneName);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            var color = m_spriteRender.color;
+            color.a = 1.0f;
+            m_spriteRender.color = color;
+            m_bReady = true;
+            Debug.Log("HIT");
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            var color = m_spriteRender.color;
+            color.a = 0.5f;
+            m_spriteRender.color = color;
+            m_bReady = false;
+            Debug.Log("noHIT");
         }
     }
 }
