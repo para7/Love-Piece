2608ed8ba069c0b4bcade2185c42f318895571c7
makia <g1821120@gmail.com>
2019-09-05 13:42:52 +0900

    Loverのタイマー機能修正完了

--- Team-C/Mote_G2Intern/Assets/chomo/Scripts/Lover.cs
+++ Team-C/Mote_G2Intern/Assets/chomo/Scripts/Lover.cs
@@ -20,6 +20,8 @@ public class Lover : MonoBehaviour
 
     private bool m_isArea;
 
+    IDisposable m_StopLover;
+
     //呼び出し時に方向を代入させる
     [HideInInspector] public Vector2 m_moveDirection { get; set; }
 
@@ -64,6 +66,12 @@ public class Lover : MonoBehaviour
 
         m_isStopped = false;
         m_isArea = false;
+
+        m_StopLover = Observable.Timer(TimeSpan.FromSeconds(m_stopTime)).Subscribe(_ =>
+        {
+            m_isStopped = false;
+            SetMoveDirection(isApproach: false);
+        });
     }
 
     public void LoverUpdate()
@@ -83,6 +91,7 @@ public class Lover : MonoBehaviour
     public void LoverAllStop()
     {
         _loverMover.Move(Vector2.zero);
+        m_StopLover.Dispose();
     }
 
     private void SetMoveDirection(bool isApproach)
@@ -115,11 +124,7 @@ public class Lover : MonoBehaviour
             /// <summary>
             /// m_stopTime秒経ったらfalseにする
             /// </summary>
-            Observable.Timer(TimeSpan.FromSeconds(m_stopTime)).Subscribe(_ =>
-            {
-                m_isStopped = false;
-                SetMoveDirection(isApproach: false);
-            }).AddTo(this);
+            m_StopLover.AddTo(this);
         }
 
         if (collision.gameObject.CompareTag("ResultArea"))
@@ -137,6 +142,9 @@ public class Lover : MonoBehaviour
 
         if (m_isStopped)
         {
+            m_StopLover.Dispose();
+            m_StopLover.AddTo(this);
+
             return;
         }
 
@@ -168,6 +176,7 @@ public class Lover : MonoBehaviour
         if (collision.gameObject.CompareTag("DeleteZone"))
         {
             LoverManager.Instance.RemoveList(this);
+            m_StopLover.Dispose();
             Destroy(this.gameObject);
         }
     }
