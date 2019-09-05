ec6680cfb57a06276a8132ae95b1074ae74e4df1
DESKTOP-6AV5CTI\narinari <the.gamut.of.human.feelings@gmail.com>
2019-09-05 12:24:48 +0900

    スコアの表示の可視化実装なう

---
+++ Team-C/Mote_G2Intern/Assets/chomo/Scripts/ObjectPoolManager.cs
@@ -0,0 +1,34 @@
+﻿using UniRx;
+using UniRx.Triggers;
+using UnityEngine;
+
+public class ObjectPoolManager : MonoBehaviour
+{
+    [SerializeField]
+    private PoolAbleObject m_poolAblePrefab;
+
+    private GameObjectPool m_gameObjectPool; //追加
+
+    void Start()
+    {
+        m_gameObjectPool = new GameObjectPool(this.transform, m_poolAblePrefab);
+
+        this.OnDestroyAsObservable().Subscribe(_ => m_gameObjectPool.Dispose());
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Space)) { OnGetScorePlayer(this.transform.position); }
+    }
+    private void OnGetScorePlayer(Vector3 objectPosition)
+    {
+        var gameObj = m_gameObjectPool.Rent();
+
+        gameObj.ShowScore(objectPosition)
+            .Subscribe(__ =>
+            {
+                Debug.Log("Return");
+                m_gameObjectPool.Return(gameObj);
+            }).AddTo(this);
+    }
+}
---
+++ Team-C/Mote_G2Intern/Assets/chomo/Scripts/PoolAbleObject.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System;
+using UniRx;
+using DG.Tweening;
+
+public class PoolAbleObject : MonoBehaviour
+{
+    [SerializeField]private double m_finishTime = 2.0f;
+
+    public IObservable<Unit> ShowScore(Vector3 position)
+    {
+        transform.position = position;
+
+        transform.DOPunchScale(transform.localScale * 1.5f, 1f);
+            
+           //1秒後にエフェクトを止めて終了
+        return Observable.Timer(TimeSpan.FromSeconds(1.0f))
+                .ForEachAsync(_ => this.gameObject.SetActive(false));
+    }
+}
